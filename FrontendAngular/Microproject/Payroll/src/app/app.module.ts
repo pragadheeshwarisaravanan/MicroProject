6d64d758878988409393f53860083b7074f46f07
Pragadheeshwari Saravanan <162480835+pragadheeshwarisaravanan@users.noreply.github.com>
2024-08-07 10:03:33 +0530

    Add files via upload

---
+++ FrontendAngular/Microproject/Payroll/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { EmployeeService } from './employee.service';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [
+    // provideClientHydration(),
+    provideHttpClient(withFetch()),
+    EmployeeService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
